2d6eb022f53b716334bfc0551082a3f34ac6a918
Alex Tymchenko <alex.tymchenko@teamdev.com>
2022-09-26 12:39:27 +0300

    Exclude extra dependencies from the generated POM for the plugin bundle.

--- mc-java-plugin-bundle/build.gradle.kts
+++ mc-java-plugin-bundle/build.gradle.kts
@@ -81,6 +81,26 @@ publishing {
              *         <artifactId>protodata</artifactId>
              *         <version>0.2.7</version>
              *         <scope>runtime</scope>
+             *
+             *
+             *         <exclusions>
+             *              <exclusion>
+             *                  <groupId>io.spine.protodata</groupId>
+             *                  <artifactId>*</artifactId>
+             *              </exclusion>
+             *              <exclusion>
+             *                  <groupId>org.jetbrains.kotlin</groupId>
+             *                  <artifactId>*</artifactId>
+             *              </exclusion>
+             *              <exclusion>
+             *                  <groupId>com.google.protobuf</groupId>
+             *                  <artifactId>*</artifactId>
+             *              </exclusion>
+             *              <exclusion>
+             *                  <groupId>io.spine.tools</groupId>
+             *                  <artifactId>*</artifactId>
+             *              </exclusion>
+             *         </exclusions>
              *    </dependency>
              * ```
              */
@@ -93,6 +113,13 @@ publishing {
                 Node(dependency, "artifactId", "protodata")
                 Node(dependency, "version", protoDataVersion)
                 Node(dependency, "scope", "runtime")
+
+                val exclusions = Node(dependency, "exclusions")
+
+                excludeGroupId(exclusions, "io.spine.protodata")
+                excludeGroupId(exclusions, "org.jetbrains.kotlin")
+                excludeGroupId(exclusions, "com.google.protobuf")
+                excludeGroupId(exclusions, "io.spine.tools")
             }
         }
     }
@@ -140,3 +167,9 @@ tasks.shadowDistTar.get().enabled = false
 tasks.shadowDistZip.get().enabled = false
 tasks.distTar.get().enabled = false
 tasks.distZip.get().enabled = false
+
+fun excludeGroupId(exclusions: Node, groupId: String) {
+    val exclusion = Node(exclusions, "exclusion")
+    Node(exclusion, "groupId", groupId)
+    Node(exclusion, "artifactId", "*")
+}
